21195b2b6052ebab38271dc69975af21b75ddafd
Dave <48577149+dfuent@users.noreply.github.com>
2020-10-13 20:43:18 -0400

    Add files via upload
    log of both price and carat

--- diamonds_cat_analysis.R
+++ diamonds_cat_analysis.R
@@ -1,5 +1,5 @@
 # STAT 6021
-# Proj 1: Diamonds
+# Proj 1: Diamonds - Categorical Variable Analyses
 
 library(MASS) # for Box-Cox
 library(dplyr)
@@ -12,27 +12,29 @@ color <- factor(color, levels = c('J', 'I', 'H', 'G', 'F', 'E', 'D'))
 cut <- factor(cut, levels = c('Good', 'Very Good', 'Ideal', 'Astor Ideal'))
 
 summary(diamonds_data)
-attach(diamonds_data) # attach
+attach(diamonds_data) # attach_
 
 diamonds_data
 head(diamonds_data)
 
-diamonds_data <- mutate(diamonds_data, log_carat = log(carat), log_t_price = log(price^(1/3)))
+diamonds_data <- mutate(diamonds_data, log_carat = log(carat), log_price = log(price)) 
+                        
+#log_cb_price = log(price^(1/3))) # don't need this
 
-diamonds_data <- diamonds_data[c('log_carat', 'clarity', 'color', 'cut', 'log_t_price')]
+diamonds_data <- diamonds_data[c('log_carat', 'clarity', 'color', 'cut', 'log_price')]
 
 head(diamonds_data)
 attach(diamonds_data)
 
-boxplot(log_carat~cut, main="Boxplot of Carat and Cut")
-boxplot(log_carat~color, main="Boxplot of Carat and Color")
-boxplot(log_carat~clarity, main="Boxplot of Carat and Clarity")
+boxplot(log_carat~cut, main="Boxplot of log Carat and Cut")
+boxplot(log_carat~color, main="Boxplot of log Carat and Color")
+boxplot(log_carat~clarity, main="Boxplot of log Carat and Clarity")
 
-boxplot(log_t_price~cut, main="Boxplot of Price and Cut")
-boxplot(log_t_price~color, main="Boxplot of Price and Cut")
-boxplot(log_t_price~clarity, main="Boxplot of Price and Cut")
+boxplot(log_price~cut, main="Boxplot of log Price and Cut")
+boxplot(log_price~color, main="Boxplot of log Price and Cut")
+boxplot(log_price~clarity, main="Boxplot of log Price and Cut")
 
-t_full <- lm(log_t_price~., data = diamonds_data)
+t_full <- lm(log_price~., data = diamonds_data)
 summary(t_full)
 anova(t_full)
 
@@ -42,9 +44,9 @@ unique(clarity)
 
 # redo scatters
 
-#p_cut <- data_t[c('log_t_price', 'cut')]
-#p_color <- data_t[c('log_t_price', 'color')]
-#p_clarity <- data_t[c('log_t_price', 'clarity')]
+#p_cut <- data_t[c('log_price', 'cut')]
+#p_color <- data_t[c('log_price', 'color')]
+#p_clarity <- data_t[c('log_price', 'clarity')]
 
 
 ## consider each cut as a subset
@@ -55,20 +57,20 @@ G<-subset(diamonds_data,cut=="Good")
 
 
 # fit separate regressions
-price_AI <- lm(log_t_price~log_carat,data=AI)
-price_I <- lm(log_t_price~log_carat,data=I)
-price_VG <- lm(log_t_price~log_carat,data=VG)
-price_G <- lm(log_t_price~log_carat,data=G)
+price_AI <- lm(log_price~log_carat,data=AI)
+price_I <- lm(log_price~log_carat,data=I)
+price_VG <- lm(log_price~log_carat,data=VG)
+price_G <- lm(log_price~log_carat,data=G)
 
 
 # Astor Ideal is the reference
 
 # Create scatters:
 
-plot(log_carat, log_t_price, main="Price by Carat and Cut, transformed model")
-points(I$log_carat, I$log_t_price, pch=2, col="blue")
-points(VG$log_carat, VG$log_t_price, pch=3, col="red")
-points(G$log_carat, G$log_t_price, pch=4, col="orange")
+plot(log_carat, log_price, main="log Price by log Carat and Cut")
+points(I$log_carat, I$log_price, pch=2, col="blue")
+points(VG$log_carat, VG$log_price, pch=3, col="red")
+points(G$log_carat, G$log_price, pch=4, col="orange")
 
 
 abline(price_AI,lty=1)
@@ -80,7 +82,7 @@ legend("topleft", c("Astor Ideal", "Ideal", "Very Good", "Good"), lty=c(1, 2, 3,
 
 cut <- factor(cut)
 
-m_cut <- lm(log_t_price~log_carat + cut, data = diamonds_data)
+m_cut <- lm(log_price~log_carat + cut, data = diamonds_data)
 
 summary(m_cut)
 
@@ -97,9 +99,9 @@ summary(pairwise)
 
 cut
 
-top <- c('Astor Ideal', 'Ideal')
+top <- c('Astor Ideal', 'Ideal') # top cut
 
-diamonds_data$cut_group <- ifelse(diamonds_data$cut %in% top, 'Top Cut', 'Bottom Cut')
+diamonds_data$cut_group <- ifelse(diamonds_data$cut %in% top, 'Top Cut', 'Bottom Cut') # bottom cut is very good and good
 
 head(diamonds_data)
 
@@ -110,14 +112,14 @@ sc<-subset(diamonds_data,cut_group=="Bottom Cut")
 head(sc)
 
 # fit separate regressions
-price_TopCut <- lm(log_t_price~log_carat,data=tc)
-price_SecondCut <- lm(log_t_price~log_carat,data=sc)
+price_TopCut <- lm(log_price~log_carat,data=tc)
+price_SecondCut <- lm(log_price~log_carat,data=sc)
 
 
 # Create scatters:
 
-plot(tc$log_carat, tc$log_t_price, col = 'blue', main="Price by Carat and Cut, transformed model")
-points(sc$log_carat, sc$log_t_price, pch=2, col="red")
+plot(tc$log_carat, tc$log_price, col = 'blue', main="Log Price by Log Carat and Grouped Cut")
+points(sc$log_carat, sc$log_price, pch=2, col="red")
 
 
 abline(price_TopCut,lty=1, col = 'blue')
@@ -135,7 +137,7 @@ attach(diamonds_data)
 
 count(diamonds_data, c(cut_group))
 
-m_cut <- lm(log_t_price~log_carat + cut_group)
+m_cut <- lm(log_price~log_carat + cut_group)
 
 summary(m_cut)
 
@@ -145,7 +147,7 @@ summary(pairwise)
 #colors()[1:50] # R colors for graphs
 
 # color:
-D <-subset(diamonds_data,color=="E") 
+D <-subset(diamonds_data,color=="D") 
 E <-subset(diamonds_data, color=="E") 
 eF<-subset(diamonds_data,color=="F") # f is a reserved variable
 G<-subset(diamonds_data,color=="G") 
@@ -154,21 +156,21 @@ I<-subset(diamonds_data,color=="I")
 J<-subset(diamonds_data,color=="J") 
 
 # fit separate regressions
-price_D <- lm(log_t_price~log_carat,data=D)
-price_E <- lm(log_t_price~log_carat,data=E)
-price_eF <- lm(log_t_price~log_carat,data=eF)
-price_G <- lm(log_t_price~log_carat,data=G)
-price_H <- lm(log_t_price~log_carat,data=H)
-price_I <- lm(log_t_price~log_carat,data=I)
-price_J <- lm(log_t_price~log_carat,data=J)
+price_D <- lm(log_price~log_carat,data=D)
+price_E <- lm(log_price~log_carat,data=E)
+price_eF <- lm(log_price~log_carat,data=eF)
+price_G <- lm(log_price~log_carat,data=G)
+price_H <- lm(log_price~log_carat,data=H)
+price_I <- lm(log_price~log_carat,data=I)
+price_J <- lm(log_price~log_carat,data=J)
 
-plot(log_carat, log_t_price, main="Price by Carat and Color")
-points(E$log_carat, E$log_t_price, pch=2, col="chartreuse")
-points(eF$log_carat, eF$log_t_price, pch=3, col="blue")
-points(G$log_carat, G$log_t_price, pch=4, col="orange")
-points(H$log_carat, H$log_t_price, pch=5, col="red")
-points(I$log_carat, I$log_t_price, pch=6, col="bisque1")
-points(J$log_carat, J$log_t_price, pch=7, col="aquamarine")
+plot(log_carat, log_price, main="Price by Carat and Color")
+points(E$log_carat, E$log_price, pch=2, col="chartreuse")
+points(eF$log_carat, eF$log_price, pch=3, col="blue")
+points(G$log_carat, G$log_price, pch=4, col="orange")
+points(H$log_carat, H$log_price, pch=5, col="red")
+points(I$log_carat, I$log_price, pch=6, col="bisque1")
+points(J$log_carat, J$log_price, pch=7, col="aquamarine")
 
 abline(price_D,lty=1, col = "black")
 abline(price_E,lty=2, col = "chartreuse")
@@ -183,7 +185,7 @@ legend("topleft", c("D", "E", "F", "G", "H", "I", "J"), lty=c(1, 2, 3, 4, 5, 6, 
 
 count(diamonds_data, c(color))
 
-best_col <- c('D', 'E', 'F', 'G', 'H')
+best_col <- c('D', 'E', 'F', 'G', 'H') # all but bottom 2
 
 diamonds_data$col_group <- ifelse(diamonds_data$color %in% best_col, 'Top Col', 'Bottom Col')
 
@@ -193,12 +195,12 @@ bottom_col <-subset(diamonds_data, col_group=="Bottom Col")
 
 
 # fit separate regressions
-price_topc <- lm(log_t_price~log_carat,data=top_col)
-price_bottomc <- lm(log_t_price~log_carat,data=bottom_col)
+price_topc <- lm(log_price~log_carat,data=top_col)
+price_bottomc <- lm(log_price~log_carat,data=bottom_col)
 
 
-plot(log_carat, log_t_price, main="Price by Carat and Color, top and bottom")
-points(bottom_col$log_carat, bottom_col$log_t_price, pch=2, col="red")
+plot(log_carat, log_price, main="log Price by log Carat and Grouped Color")
+points(bottom_col$log_carat, bottom_col$log_price, pch=2, col="red")
 
 
 abline(price_topc,lty=1, col = "black")
@@ -219,7 +221,7 @@ attach(diamonds_data)
 
 count(diamonds_data, c(col_group))
 
-m_col <- lm(log_t_price~log_carat + col_group)
+m_col <- lm(log_price~log_carat + col_group)
 
 summary(m_col)
 
@@ -238,24 +240,24 @@ SI1 <-subset(diamonds_data,clarity=="SI1")
 SI2 <-subset(diamonds_data,clarity=="SI2") 
 
 # fit separate regressions
-price_FL <- lm(log_t_price~log_carat,data=FL)
-price_IF <- lm(log_t_price~log_carat,data=IF)
-price_VVS1 <- lm(log_t_price~log_carat,data=VVS1)
-price_VVS2 <- lm(log_t_price~log_carat,data=VVS2)
-price_VS1 <- lm(log_t_price~log_carat,data=VS1)
-price_VS2 <- lm(log_t_price~log_carat,data=VS2)
-price_SI1 <- lm(log_t_price~log_carat,data=SI1)
-price_SI2 <- lm(log_t_price~log_carat,data=SI2)
+price_FL <- lm(log_price~log_carat,data=FL)
+price_IF <- lm(log_price~log_carat,data=IF)
+price_VVS1 <- lm(log_price~log_carat,data=VVS1)
+price_VVS2 <- lm(log_price~log_carat,data=VVS2)
+price_VS1 <- lm(log_price~log_carat,data=VS1)
+price_VS2 <- lm(log_price~log_carat,data=VS2)
+price_SI1 <- lm(log_price~log_carat,data=SI1)
+price_SI2 <- lm(log_price~log_carat,data=SI2)
 
 
-plot(log_carat, log_t_price, main="Price by Carat and Clarity")
-points(IF$log_carat, IF$log_t_price, pch=2, col="chartreuse")
-points(VVS1$log_carat, VVS1$log_t_price, pch=3, col="aquamarine")
-points(VVS2$log_carat, VVS2$log_t_price, pch=4, col="burlywood")
-points(VS1$log_carat, VS1$log_t_price, pch=5, col="red")
-points(VS2$log_carat, VS2$log_t_price, pch=6, col="bisque1")
-points(SI1$log_carat, SI1$log_t_price, pch=7, col="blue")
-points(SI2$log_carat, SI2$log_t_price, pch=8, col="orange")
+plot(log_carat, log_price, main="log Price by log Carat and Clarity")
+points(IF$log_carat, IF$log_price, pch=2, col="chartreuse")
+points(VVS1$log_carat, VVS1$log_price, pch=3, col="aquamarine")
+points(VVS2$log_carat, VVS2$log_price, pch=4, col="burlywood")
+points(VS1$log_carat, VS1$log_price, pch=5, col="red")
+points(VS2$log_carat, VS2$log_price, pch=6, col="bisque1")
+points(SI1$log_carat, SI1$log_price, pch=7, col="blue")
+points(SI2$log_carat, SI2$log_price, pch=8, col="orange")
 
 
 
@@ -299,15 +301,15 @@ head(S)
 
 
 # fit separate regressions
-price_fl <- lm(log_t_price~log_carat,data=FL)
-price_vs <- lm(log_t_price~log_carat,data=VS)
-price_s <- lm(log_t_price~log_carat,data=S)
+price_fl <- lm(log_price~log_carat,data=FL)
+price_vs <- lm(log_price~log_carat,data=VS)
+price_s <- lm(log_price~log_carat,data=S)
 
 
 
-plot(log_carat, log_t_price, main="Price by Carat and Clarity")
-points(VS$log_carat, VS$log_t_price, pch=2, col="red")
-points(S$log_carat, S$log_t_price, pch=3, col="blue")
+plot(log_carat, log_price, main="log Price by log Carat and Grouped Clarity")
+points(VS$log_carat, VS$log_price, pch=2, col="red")
+points(S$log_carat, S$log_price, pch=3, col="blue")
 
 
 abline(price_fl,lty=1, col = "black")
@@ -321,17 +323,19 @@ legend("topleft", c("Flawless", 'Very Slightly Included', 'Included'), lty=c(1, 
 grid()
 
 
-m_clarity <- lm(log_t_price~log_carat + cl_group)
+m_clarity <- lm(log_price~log_carat + cl_group)
 
 
 pairwise<-glht(m_clarity, linfct = mcp(cl_group= "Tukey"))
 summary(pairwise)
 
 
-boxplot(log_carat~cut_group, main="Boxplot of Carat and Cut")
-boxplot(log_carat~col_group, main="Boxplot of Carat and Color")
-boxplot(log_carat~cl_group, main="Boxplot of Carat and Clarity")
+boxplot(log_carat~cut_group, main="Boxplot of log Carat and Cut")
+boxplot(log_carat~col_group, main="Boxplot of log Carat and Color")
+boxplot(log_carat~cl_group, main="Boxplot of log Carat and Clarity")
 
-boxplot(log_t_price~cut_group, main="Boxplot of Price and Cut")
-boxplot(log_t_price~col_group, main="Boxplot of Price and Color")
-boxplot(log_t_price~cl_group, main="Boxplot of Price and Clarity")
+boxplot(log_price~cut_group, main="Boxplot of log Price and Cut")
+boxplot(log_price~col_group, main="Boxplot of log Price and Color")
+boxplot(log_price~cl_group, main="Boxplot of log Price and Clarity")
+
+
